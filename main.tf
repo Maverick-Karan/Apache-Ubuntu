a1e046bd9df0217cf3798fae0ac4e80ef8f7e3d7
Karan <>
2023-01-29 14:00:13 -0500

    test10

--- main.tf
+++ main.tf
@@ -2,12 +2,9 @@ provider "aws" {
    region = "us-east-1"
 }
 
-
-
-
-resource "aws_s3_object" "object" {
-  bucket = "test211293"
-  key    = "buddha.jpg"
-  source = "./buddha.jpg"
-  #etag = filemd5("./buddha.jpg")
+resource "aws_s3_bucket" "bucket" {
+   bucket = "testdynamoDB1007"
+   tags = {
+      description = "Testing"
+   }
 }
