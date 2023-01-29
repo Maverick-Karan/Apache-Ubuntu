640115357d3c125803f8e0343998bc77af5d4fd8
Karan <>
2023-01-29 12:48:35 -0500

    terraform9

--- main.tf
+++ main.tf
@@ -9,5 +9,5 @@ resource "aws_s3_object" "object" {
   bucket = "test211293"
   key    = "buddha.jpg"
   source = "./buddha.jpg"
-  etag = filemd5("./buddha.jpg")
+  #etag = filemd5("./buddha.jpg")
 }
