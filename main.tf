e5ab7a84a1af82e9b62a08583885e9e13aa9c9dd
Karan <>
2023-01-29 14:03:46 -0500

    test12

--- main.tf
+++ main.tf
@@ -2,9 +2,3 @@ provider "aws" {
    region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "bucket" {
-   bucket = "testing211293"
-   tags = {
-      description = "Testing"
-   }
-}
--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 terraform {
    backend "s3" {
       bucket = "testing211293"
-      key = "backend/terraform.tfstate"
+      key = "./terraform.tfstate"
       region = "us-east-1"
       dynamodb_table = "state-locking" 
    }
