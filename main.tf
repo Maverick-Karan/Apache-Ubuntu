5e93c231044f6fc44709039a2c7c9f5ea0950325
Karan <>
2023-01-29 00:57:09 -0500

    terraform1

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
-provider "aws" {
-   region = "us-east-1"
-   access_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
-   secret_key = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
-}
+
 
 resource "aws_s3_bucket" "bucket" {
    bucket = "test211293"
