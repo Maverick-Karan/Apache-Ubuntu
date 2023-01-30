686afa05efd2b49f86679dc2d0851755fac86946
Karan <>
2023-01-29 23:17:34 -0500

    test21

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "webserver" {
    ami = "ami-0aa7d40eeae50c9a9"
    instance_type = "t2.micro"
    tags = {
-      Name = "webserver"
+      Name = "webbserverr"
       Description = "Nginx on ubuntu"
    }
 
