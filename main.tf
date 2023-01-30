3367fddab8db038e3aec1ebb130e0b7117238db2
Karan <>
2023-01-29 23:57:05 -0500

    test24

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 resource "aws_instance" "webserver" {
    ami = "ami-0aa7d40eeae50c9a9"
    instance_type = "t2.micro"
-   
-
-   provisioner "remote-exec" {
-      inline = [ "sudo apt update",
-                 "sudo apt install nginx -y",
-                 "systemctl enable nginx",
-                 "systemctl start nginx",
-               ]
+   tags = {
+      Name = "webserver"
+      Description = "Nginx on ubuntu"
    }
 
-   connection {
-      type = "ssh"
-      host = self.public_ip
-      user = "ubuntu"
-      private_key = "webserver"
-   }
+   user_data = <<-EOF
+               #!/bin/bash
+               sudo apt update
+               sudo apt install nginx -y
+               systemctl enable nginx
+               systemctl start nginx
+               EOF
+
+   key_name = "webserver"
+   vpc_security_group_ids = [aws_security_group.ssh-access.id]
+}
 
    key_name = "webserver"
    vpc_security_group_ids = [aws_security_group.ssh-access.id]
