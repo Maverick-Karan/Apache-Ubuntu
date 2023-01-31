2c2f19e0973c1ba3d4eb6fcfca4d95ff571407f0
Karan <>
2023-01-30 19:23:57 -0500

    test31

--- main.tf
+++ main.tf
@@ -11,8 +11,13 @@ resource "aws_instance" "webserver" {
                sudo yum update -y
                sudo yum install -y httpd
                sudo systemctl enable httpd
-               sudo service httpd start 
+               sudo service httpd start
+               sudo echo '<h1>Welcome to StackSimplify - APP-1</h1>' | sudo tee /var/www/html/index.html
+               sudo mkdir /var/www/html/app1
+               sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Welcome to Stack Simplify - APP-1</h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
+               sudo curl http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/app1/metadata.html
                EOF
+
 
    key_name = "webserver"
    vpc_security_group_ids = [aws_security_group.ssh-access.id, aws_security_group.public_access.id]
